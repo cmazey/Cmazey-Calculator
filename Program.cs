53d3746ad557e350a4288b8f5441a0bc5119e9e9
cmazey <cmazey10@outlook.com>
2023-09-11 12:22:13 +0000

    Update "Program.cs"

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 Console.WriteLine ("--- CMAZEY CALCULATOR ---");
-Console.WriteLine("ALPHA v1.06");
+Console.WriteLine("ALPHA v1.07");
 
 Console.WriteLine("\nType /help to show all the available commands!");
 
